5729e6c4bb57d8746034ed11f57f7fd7c8335304
k-hatano <kent.ruffle.mgj626@gmail.com>
2022-10-23 23:40:49 +0900

    Support halfwidth katakana

--- script.js
+++ script.js
@@ -12,58 +12,44 @@ function generateQRCodeClicked() {
     document.getElementById('qr').innerHTML = qr.createImgTag(4);
 }
 
-function generateDataForQRCode(programName, source) {
-    console.log("source : " + source);
+function generateDataForQRCode(programName, utfSource) {
+    let sjisSource = Encoding.convert(Encoding.stringToCode(utfSource), {
+      to: 'SJIS',
+      from: 'UNICODE'
+    });
+    var source = Encoding.codeToString(sjisSource);
+
     let petcSource = "PETC0300RPRG";
     petcSource += leBytesForNum(0);
     petcSource += leBytesForNum(0);
     petcSource += leBytesForNum(source.length);
     petcSource += source;
-    console.log("petcSource : " + petcSource);
-    console.log("petcSource.length : " + petcSource.length);
-    console.log("petcSource$ : " + stringToUnicodeEscapedStringLiteral(petcSource));
     
-    let deflate = new Zlib.Deflate(stringToByteArray(petcSource), {compressionType: Zlib.Deflate.CompressionType.DYNAMIC});
+    let deflate = new Zlib.Deflate(stringToByteArray(petcSource), {
+        compressionType: Zlib.Deflate.CompressionType.DYNAMIC
+    });
     let petcSourceRawCompressed = deflate.compress();
-    console.log("petcSourceRawCompressed : " + petcSourceRawCompressed);
-    let petcSourceCompressed = bytesToRawString(petcSourceRawCompressed);
-    console.log("petcSourceCompressed : " + petcSourceCompressed);
-    console.log("petcSourceCompressed.length : " + petcSourceCompressed.length);
-    console.log("petcSourceCompressed$ : " + stringToUnicodeEscapedStringLiteral(petcSourceCompressed));
+    let petcSourceCompressed = bytesToString(petcSourceRawCompressed);
 
-    let rprgSource = stringPaddingNull(programName, 8);
+    let rprgSource = stringPaddedNull(programName, 8);
     rprgSource += "RPRG";
     rprgSource += leBytesForNum(petcSourceCompressed.length);
     rprgSource += leBytesForNum(petcSource.length);
     rprgSource += petcSourceCompressed;
-    console.log("rprgSource : " + rprgSource);
-    console.log("rprgSource.length : " + rprgSource.length);
 
     let hash = md5(stringToByteArray(rprgSource));
-    console.dir(hash);
 
     let partSource = "PT";
     partSource += String.fromCharCode(0x01);
     partSource += String.fromCharCode(0x01);
-    partSource += bytesFromMD5Hash(hash);
-    partSource += bytesFromMD5Hash(hash);
+    partSource += md5HashToBytes(hash);
+    partSource += md5HashToBytes(hash);
     partSource += rprgSource;
-    console.log("rprgSource$ : " + stringToUnicodeEscapedStringLiteral(rprgSource));
-    console.log("partSource$ : " + stringToUnicodeEscapedStringLiteral(partSource));
-    console.log("partSource.length : " + partSource.length);
 
     return partSource.substring(0, 630);
 }
 
 /* Utils */
-
-function stringToUnicodeEscapedStringLiteral(string) {
-    let result = "";
-    for (let i = 0; i < string.length; i++) {
-        result += "\\x" + ("00" + string.charCodeAt(i).toString(16)).slice(-2);
-    } 
-    return result;
-}
 
 function stringToByteArray(string) {
     let result = [];
@@ -73,7 +59,7 @@ function stringToByteArray(string) {
     return result;
 }
 
-function bytesFromMD5Hash(hash) {
+function md5HashToBytes(hash) {
     let result = '';
     for (let i = 0; i < 16; i++) {
         result += String.fromCharCode(parseInt(hash.substring(i * 2, i * 2 + 2), 16));
@@ -81,7 +67,7 @@ function bytesFromMD5Hash(hash) {
     return result;
 }
 
-function bytesToRawString(bytes) {
+function bytesToString(bytes) {
     let result = '';
     for (let i = 0; i < bytes.length; i++) {
         result += String.fromCharCode(bytes[i] & 0xff);
@@ -89,7 +75,7 @@ function bytesToRawString(bytes) {
     return result;
 }
 
-function stringPaddingNull(str, len) {
+function stringPaddedNull(str, len) {
     let result = str;
     while (result.length < len) {
         result += "\0";
